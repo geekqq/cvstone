a0c5f41abaf955412fe9e5d3194dd56207669096
geekqq <jinwei1014@gmail.com>
2024-01-15 18:01:21 -0800

    add new content

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -28,35 +28,35 @@ export const RESUME_DATA = {
     "Full Stack Engineer focused on building products with extra attention to detail",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  avatarUrl: "https://t.ly/2CbQu",
+  personalWebsiteUrl: "https://stoni.me", "https://stonehoo.vercel.app"
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "jinwei1014@gmail.com",
+    tel: "+1 628-233-6666",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/geekqq",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/stonehoo/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/stonehoo",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Zhejiang Jiaxing University",
+      degree: "Bachelor's Degree in Business and English",
+      start: "2003",
+      end: "2007",
     },
   ],
   work: [
